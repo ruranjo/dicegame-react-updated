ee3b38eaac0b24ac2709fd9c12f00e2c52d0920c
ruranjo <rubens.rangel.job@gmail.com>
2023-08-24 08:12:25 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import StartGame from './components/StartGame'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <StartGame/>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+/*
+ */
